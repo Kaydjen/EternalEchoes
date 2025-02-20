a20fc104afcc2ce746397ae778f8a56e8bcb78c2
Kaydjen <ravnerk@gmail.com>
2025-02-20 20:14:12 +0100

    Update

--- Assets/Scripts/Interaction/ShowClue.cs
+++ Assets/Scripts/Interaction/ShowClue.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 
-public class ShowClue : MonoBehaviour, IInteraction
+public class ShowClue : MonoBehaviour, IInteraction, IUpdate
 {
     public void HoverEnter()
     {
         this.transform.GetChild(0).gameObject.SetActive(true);
         Debug.Log("Enter");
+        Updater.Instance.RegisterUpdate(this, Updater.UpdateType.InitialUpdate);
+        this.transform.GetChild(0).transform.localRotation = Quaternion.LookRotation(this.transform.GetChild(0).transform.position - Camera.main.transform.position);
     }
 
     public void HoverExit()
     {
         this.transform.GetChild(0).gameObject.SetActive(false);
         Debug.Log("Exit");
+        Updater.Instance.UnregisterUpdate(this, Updater.UpdateType.InitialUpdate);
+    }
+    public void PerformInitialUpdate()
+    {
+        this.transform.GetChild(0).transform.localRotation = Quaternion.LookRotation(this.transform.GetChild(0).transform.position - Camera.main.transform.position);
+    }
+    public void PerformLateUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+    public void PerformPreUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+    public void PerformUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+    public void PerformFinalUpdate()
+    {
+        throw new System.NotImplementedException();
     }
 }
