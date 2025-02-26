5be7789a84bc20d8fe2d8d2c7f168f567e624637
Kaydjen <ravnerk@gmail.com>
2025-02-26 23:45:31 +0100

    Update

--- Assets/Scripts/Interaction/MainLogic/CharacterOptions.cs
+++ Assets/Scripts/Interaction/MainLogic/CharacterOptions.cs
@@ -4,16 +4,19 @@ public class CharacterOptions : MonoBehaviour
 {
     #region VARIABLES
     private static Transform _selectedCharacter;
+    private static GameObject _manu;
     #endregion
     #region PUBLIC METHODS
     public static void EnableChoise(Transform characterObj)
     {
         InputHandler.Instance.ActivateOptionsNumbersMap();
         _selectedCharacter = characterObj;
+        _manu.SetActive(true);
     }
     public static void DisableChoise()
     {
         InputHandler.Instance.ActivateDefNumbersMap();
+        _manu.SetActive(false);
     }
     #endregion
     #region PRIVATE METHODS
@@ -31,6 +34,7 @@ public class CharacterOptions : MonoBehaviour
     {
         InputHandler.OnOptionsOne.AddListener(SwitchToNewCharacter);
         InputHandler.OnOptionsTwo.AddListener(SayHello);
+        _manu = this.transform.GetChild(0).transform.gameObject;
     }
     #endregion
 }
