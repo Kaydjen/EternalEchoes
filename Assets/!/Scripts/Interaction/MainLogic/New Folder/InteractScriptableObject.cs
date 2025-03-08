2b85a85a325532e29b893172b904f11dc86d9406
Kaydjen <ravnerk@gmail.com>
2025-03-08 13:59:07 +0100

    Update

--- Assets/!/Scripts/Interaction/MainLogic/New Folder/InteractScriptableObject.cs
+++ Assets/!/Scripts/Interaction/MainLogic/New Folder/InteractScriptableObject.cs
@@ -3,7 +3,41 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "InteractionObjectsParameters", menuName = "ScriptableObjects/InteractionObjectsParameters", order = 1)]
 public class InteractScriptableObject : ScriptableObject
 {
-    public int OptionsCount = 0;
+    //public int OptionsCount = 0;
+    [Header("Don't forget to make the Description and Icon counts the same")]
+    public string[] Description =
+    {
+        ">",
+        ">",
+        ">",
+        ">",
+        ">",
+    };
+    public Sprite[] Icons;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ 
     public string[] Description = 
     {
         "1.",
@@ -11,9 +45,10 @@ public class InteractScriptableObject : ScriptableObject
         "3.",
         "4.",
         "5.",
-/*        "6.",
+        "6.",
         "7.",
         "8.",
-        "9.",*/
+        "9.",
     };
-}
+
+*/
---
+++ Assets/!/Scripts/UI/Tabs.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Tabs : MonoBehaviour
+{
+    [SerializeField] private Transform _tabsPosition;
+    [SerializeField] private GameObject _tabPref;
+    private GameObject _instant;
+    private void Start()
+    {
+        StartCoroutine(nameof(SetTabs));
+    }
+    public IEnumerator SetTabs()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            _instant = Instantiate(_tabPref, _tabsPosition);
+            _instant.transform.SetParent(_tabsPosition);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
