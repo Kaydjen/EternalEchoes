401295a24742560d75d411a849db429958ebfeec
Kaydjen <ravnerk@gmail.com>
2025-03-14 17:36:28 +0100

    UDpate

--- Assets/!/Scripts/Weapons/Gun.cs
+++ Assets/!/Scripts/Weapons/Gun.cs
@@ -1,17 +1,9 @@
 using UnityEngine;
 
-public class Gun : AbstractWeapon, IGameplayModeSwitcher
+public class Gun : AbstractWeapon
 {
     public override void Attack()
     {
         Debug.Log("BAM!!!");
-    }
-    public void ForManualMode()
-    {
-        InputHandler.OnAttackLMB.AddListener(Attack);
-    }
-    public void ForAIMode()
-    {
-        InputHandler.OnAttackLMB.RemoveListener(Attack);
     }
 }
---
+++ Assets/!/Scripts/Weapons/MainLogic/WeaponManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class WeaponManager : MonoBehaviour, IGameplayModeSwitcher
+{
+    public WeaponScriptableAbstract Data;
+    public AbstractWeapon AbstractWeapon;
+
+    public void Attack()
+    {
+        AbstractWeapon.Attack();
+    }
+    public void ForManualMode()
+    {
+        InputHandler.OnAttackLMB.AddListener(Attack);
+    }
+    public void ForAIMode()
+    {
+        InputHandler.OnAttackLMB.RemoveListener(Attack);
+    }
+}
+
