9f322e2facb0050226cd75b7556e6befd9333172
Kaydjen <ravnerk@gmail.com>
2025-03-15 23:44:41 +0100

    udpate

--- Assets/!/Scripts/Weapons/Gun.cs
+++ Assets/!/Scripts/Weapons/Gun.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class Gun : AbstractWeapon
 {
     private GunScriptable _data; 
-
-    private void Start() => _data = Data as GunScriptable;
+    protected override void Start() => _data = Data as GunScriptable;
     public override void Attack()
     {
         Debug.Log("BAM!!! - " + _data.Damage);
--- Assets/!/Scripts/Weapons/MainLogic/AbstractWeapon.cs
+++ Assets/!/Scripts/Weapons/MainLogic/AbstractWeapon.cs
@@ -4,4 +4,5 @@ public abstract class AbstractWeapon: MonoBehaviour
 {
     public WeaponScriptableAbstract Data; 
     public abstract void Attack();
+    protected abstract void Start();
 }
