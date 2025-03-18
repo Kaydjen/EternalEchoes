b28a30baedf7f8fe7cb2840ddacf681e4dfa739b
Kaydjen <ravnerk@gmail.com>
2025-03-18 21:23:05 +0100

    qwq

---
+++ Assets/!/Scripts/Test/ReloadGun.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+class ReloadGun : MonoBehaviour, IReload, IGameplayModeSwitcher
+{
+    public void Reload()
+    {
+
+    }
+    public void ForAIMode()
+    {
+
+    }
+    public void ForManualMode()
+    {
+
+    }
+}
