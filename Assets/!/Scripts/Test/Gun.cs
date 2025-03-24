7dc941029c5dee376478bf702490469f555804ef
Іля <illapalamarcuk78@gmail.com>
2025-03-24 17:05:00 +0200

    TochnoRabotae

--- Assets/!/Scripts/Test/Gun.cs
+++ Assets/!/Scripts/Test/Gun.cs
@@ -51,7 +51,7 @@ class Gun : MonoBehaviour, IAttack
         else
         {
             CastRay();
-            CameraShake.Instance.Anus(0.1f, 0.1f);
+            CameraShake.Instance.Anus(0.5f, 0.1f);
         }
     }
 }
