d0c739948bf4bcab837319c58b683e6aa305d26d
Іля <illapalamarcuk78@gmail.com>
2025-03-24 16:36:21 +0200

    dfvghjkk

--- Assets/!/Scripts/Camera/CameraShake.cs
+++ Assets/!/Scripts/Camera/CameraShake.cs
@@ -6,21 +6,18 @@ public class CameraShake : MonoBehaviour
 {
     [SerializeField] private float _duration;
     [SerializeField] private float _magnitude;
-    public Vector3 ogPos;
-    private Quaternion ogRot;
+    private Vector3 ogPos;
 
     public static CameraShake Instance;
 
     private void Awake()
     {
         Instance = this;
-        
     }
 
     public void Anus(float duration, float magnitude)
     {
         ogPos = transform.localPosition;
-        ogRot = transform.localRotation;
         _duration = duration;
         _magnitude = magnitude;
         StartCoroutine(Shake());
@@ -32,15 +29,10 @@ public class CameraShake : MonoBehaviour
 
         while (elapsed < _duration) 
         {
-            float xPos = Random.Range(-1f, 1f) * _magnitude;
             float yPos = Random.Range(-1f, 1f) * _magnitude;
-
-            float xRot = Random.Range(-20f, 20f) * _magnitude;
-            float yRot = Random.Range(-20f, 20f) * _magnitude;
+            float xPos = Random.Range(-1f, 1f) * _magnitude;
 
             transform.localPosition = ogPos + new Vector3(xPos, yPos);
-
-            transform.localRotation = transform.localRotation * Quaternion.Euler(xRot, yRot, 0f);
 
             elapsed += Time.deltaTime;
 
@@ -48,6 +40,5 @@ public class CameraShake : MonoBehaviour
         }
 
         transform.localPosition = ogPos;
-        transform.localRotation = ogRot;
     }
 }
