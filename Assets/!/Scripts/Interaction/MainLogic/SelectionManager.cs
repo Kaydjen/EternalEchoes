9e51c119413b16d330f9be401b14b32e105999d7
Kaydjen <ravnerk@gmail.com>
2025-03-26 23:13:01 +0100

    ss

--- Assets/!/Scripts/Interaction/MainLogic/SelectionManager.cs
+++ Assets/!/Scripts/Interaction/MainLogic/SelectionManager.cs
@@ -1,13 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SelectionManager : MonoBehaviour
 {
+    private HashSet<HighlightActivator> _selectedObjects = new();
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent(out HighlightActivator highlight)) highlight.Enable();
+        if (other.TryGetComponent(out HighlightActivator highlight))
+        {
+            highlight.Enable();
+            _selectedObjects.Add(highlight);
+        }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.TryGetComponent(out HighlightActivator highlight)) highlight.Disable();
+        if (other.TryGetComponent(out HighlightActivator highlight))
+        {
+            highlight.Disable();
+            _selectedObjects?.Remove(highlight);
+        }
+    }
+    private void OnEnable()
+    {
+        foreach (HighlightActivator el in _selectedObjects)
+            el.Disable();
+        _selectedObjects.Clear();
     }
 }
