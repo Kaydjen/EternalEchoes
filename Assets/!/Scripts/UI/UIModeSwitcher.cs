bafed023635c5212c26c43dffdcf968846e8a707
Kaydjen <ravnerk@gmail.com>
2025-03-30 22:31:11 +0200

    ss

--- Assets/!/Scripts/UI/UIModeSwitcher.cs
+++ Assets/!/Scripts/UI/UIModeSwitcher.cs
@@ -7,18 +7,18 @@ public class UIModeSwitcher : MonoBehaviour
     [SerializeField] private GameObject _TopDownMenu;
     private void EnableFPVMenu() 
     {
-        _FPVMenu.SetActive(true);
         DisableAll();
+        _FPVMenu.SetActive(true);
     }
     private void EnableIsometricMenu()
     {
-        _IsometricMenu.SetActive(true);
         DisableAll();
+        _IsometricMenu.SetActive(true);
     }   
     private void Enable_TopDownMenu()
     {
-        _TopDownMenu.SetActive(true);
         DisableAll();
+        _TopDownMenu.SetActive(true);
     }
     private void DisableAll()
     {
@@ -31,5 +31,6 @@ public class UIModeSwitcher : MonoBehaviour
         CameraSwitcher.OnFPV_Enable.AddListener(EnableFPVMenu);
         CameraSwitcher.OnIsometricV_Enable.AddListener(EnableIsometricMenu);
         CameraSwitcher.OnTopDownV_Enable.AddListener(Enable_TopDownMenu);
+        EnableFPVMenu();
     }
 }
