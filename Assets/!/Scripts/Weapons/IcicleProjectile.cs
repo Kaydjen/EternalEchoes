3546298f815bc03b05cdf5bc77e2cc02cb7c29a8
Kaydjen <ravnerk@gmail.com>
2025-03-30 20:06:15 +0200

    sssssss

--- Assets/!/Scripts/Weapons/IcicleProjectile.cs
+++ Assets/!/Scripts/Weapons/IcicleProjectile.cs
@@ -14,6 +14,7 @@ public class IcicleProjectile : MonoBehaviour
 
     private void OnEnable()
     {
+        if (AimDirection.Direction == null) return;
         _rb.velocity = Vector3.zero;
         transform.rotation = Quaternion.LookRotation(AimDirection.Direction.forward);
         _rb.AddForce(AimDirection.Direction.forward * _speed, ForceMode.Impulse);
