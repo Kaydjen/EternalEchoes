ec0e2edb90ec97eb45eb04710d0b1b215e376249
Kaydjen <ravnerk@gmail.com>
2025-04-02 17:23:37 +0200

    ss

---
+++ Assets/!/Scripts/UI/IconHoverEffect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class IconHoverEffect : MonoBehaviour
+{
+    [SerializeField] private Image _mainIcon;
+    [SerializeField] private Color _mainIconColor;
+    [SerializeField] private Color _iconHighlightedColor;
+    [SerializeField] private Color _iconPressedColor;
+    public void OnPointerEnter()
+    {
+        _mainIcon.color = _iconHighlightedColor;
+    }
+    public void OnPointerExit()
+    {
+        _mainIcon.color = _mainIconColor;
+    }
+    private void Awake()
+    {
+        OnPointerExit();
+        _mainIcon.gameObject.SetActive(true);
+    }
+}
