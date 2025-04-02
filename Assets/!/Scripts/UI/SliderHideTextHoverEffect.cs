13e3cea497d053f7e2c9efc3d39df72224f424ac
Kaydjen <ravnerk@gmail.com>
2025-04-02 18:29:09 +0200

    ssss

---
+++ Assets/!/Scripts/UI/SliderHideTextHoverEffect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class SliderHideTextHoverEffect : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    public void OnPointerEnter()
+    {
+        _slider.transform.GetComponent<Animator>().SetTrigger("HoverStart");
+    }
+    public void OnPointerExit()
+    {
+        _slider.transform.GetComponent<Animator>().SetTrigger("HoverEnd");
+    }
+    private void Awake()
+    {
+        _slider.value = 0;
+    }
+}
