43ce65f2c3161a39267d99ef13543e97cadcdb5f
Kaydjen <ravnerk@gmail.com>
2025-04-02 19:00:36 +0200

    sss

--- Assets/!/Scripts/UI/UIAlphaController.cs
+++ Assets/!/Scripts/UI/UIAlphaController.cs
@@ -4,26 +4,20 @@ using UnityEngine.UI;
 
 public class UIAlphaController : MonoBehaviour
 {
-    public Image Main;
-    public Image Highlighted;
-    public Image Pressed;
+    public Image Icon;
     public TMP_Text Text;
     private Color _colorMain = new Color();
-    private Color _colorHighlighted = new Color();
-    private Color _colorPressed = new Color();
     private Color _colorText = new Color();
     private void Awake()
     {
-        _colorMain = Main.color;
-        _colorHighlighted = Highlighted.color;
-        _colorPressed = Pressed.color;
+        _colorMain = Icon.color;
         _colorText = Text.color;
     }
     public void ChangeAlpha(float value)
     {
         _colorMain.a = value;
         _colorText.a = value;
-        Main.color = _colorMain;
+        Icon.color = _colorMain;
         Text.color = _colorText;
     }
 }
