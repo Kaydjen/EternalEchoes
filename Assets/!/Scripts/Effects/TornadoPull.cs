0dad28ffac63e82cf7362c8cba0c4dc7570eef1f
Іля <illapalamarcuk78@gmail.com>
2025-04-05 21:08:00 +0300

    укренкуецкнгнлшгенкгшгдлноерк

--- Assets/!/Scripts/Effects/TornadoPull.cs
+++ Assets/!/Scripts/Effects/TornadoPull.cs
@@ -7,18 +7,17 @@ public class TornadoPull : MonoBehaviour
     public static TornadoPull Instance;
 
     [SerializeField] private float pullSpeed = 5f;
-    [SerializeField] private float damageInterval = 0.5f;
     private void Awake()
     {
         Instance = this;
     }
 
-    public void Pull(NavMeshAgent agent, GameObject tornado, int damage)
+    public void Pull(NavMeshAgent agent, GameObject tornado)
     {
-        StartCoroutine(PullCoroutine(agent, tornado, damage));
+        StartCoroutine(PullCoroutine(agent, tornado));
     }
 
-    private IEnumerator PullCoroutine(NavMeshAgent agent, GameObject tornado, int damage)
+    private IEnumerator PullCoroutine(NavMeshAgent agent, GameObject tornado)
     {
         if (agent == null) yield break;
 
@@ -27,19 +26,9 @@ public class TornadoPull : MonoBehaviour
         while (tornado.activeSelf)
         {
             agent.transform.position = Vector3.MoveTowards(agent.transform.position, tornado.transform.position, pullSpeed * Time.deltaTime);
-            StartCoroutine(ApplyDamage(agent, damage));
             yield return null;
         }
 
         if (agent != null) agent.enabled = true;
-    }
-    private IEnumerator ApplyDamage(NavMeshAgent agent, int damage)
-    {
-        yield return new WaitForSeconds(damageInterval);
-        IDamageable damageable = agent.gameObject.GetComponent<IDamageable>();
-        if (damageable != null)
-        {
-            damageable.GetDamage(damage);
-        }
     }
 }
--- Assets/!/Scripts/Weapons/Projectile/TornadoProjectile.cs
+++ Assets/!/Scripts/Weapons/Projectile/TornadoProjectile.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
 
 public class TornadoProjectile : MonoBehaviour
@@ -6,14 +8,17 @@ public class TornadoProjectile : MonoBehaviour
     private Rigidbody _rb;
     [SerializeField] private float _speed;
     [SerializeField] private int _damage;
+    [SerializeField] private float _damageInterval;
 
+    private List<IDamageable> _enemies = new List<IDamageable>();
+    private Coroutine _korutinaYakaNanositDamagProtivnikam;
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
     }
     private void OnEnable()
     {
-        
+        _korutinaYakaNanositDamagProtivnikam = StartCoroutine(DamageDealer());
         if (AimDirection.Direction == null) return;
         _rb.velocity = Vector3.zero;
         _rb.AddForce(LookOrientation.Direction.forward * _speed, ForceMode.Impulse);
@@ -23,13 +28,26 @@ public class TornadoProjectile : MonoBehaviour
     {
         if (other.CompareTag("Enemy"))
         {
+            _enemies.Add(other.gameObject.GetComponent<IDamageable>());
             NavMeshAgent agent = other.GetComponent<NavMeshAgent>();
-            TornadoPull.Instance.Pull(agent, gameObject, _damage);
+            TornadoPull.Instance.Pull(agent, gameObject);
         }
+        
+    }
+
+    private IEnumerator DamageDealer()
+    {
+        yield return new WaitForSeconds(_damageInterval);
+        foreach (var enemy in _enemies)
+        {
+            enemy.GetDamage(_damage);
+        }
+        _korutinaYakaNanositDamagProtivnikam = StartCoroutine(DamageDealer());
     }
 
     public void OnParticleSystemStopped()
     {
+        StopCoroutine(_korutinaYakaNanositDamagProtivnikam);
         TornadoPool.Instance.Return(this.transform);
     }
 
